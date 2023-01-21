646b510d0146c68a8b9a0d133d4dbb181d559091
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-01-21 18:27:46 +0545

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Navbar from './Navbar'
 export default function Home() {
-  <Navbar/>
+  <h1> you</h1>
   return (
     <div className={styles.container}>
       <Head>
