75ea4718d8e7dbb96952c30c7e3b34cfb57f18d0
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-01-21 18:09:55 +0545

    1 file modified, 2 added

---
+++ pages/Navbar.js
@@ -0,0 +1,14 @@
+import  "../styles/Navbar.css"
+const Navbar=()=>{
+return(
+  <>
+  <nav>
+    <a href="/Components/home" className="">Home</a>
+    <a href="/Components/about" className="">About</a>
+    <a href="/Components/Menu" className="">Menu</a>
+    <a href="/Components/FAQ" className="">FAQ</a>
+  </nav>
+  </>
+)
+}
+export default Navbar;
