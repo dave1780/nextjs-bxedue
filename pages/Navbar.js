93944159b7c38223981bd31988c1cd7ea14d5a27
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-01-22 18:45:50 +0545

    2 files modified

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -2,12 +2,14 @@ import styles1 from  "../styles/Navbar.module.css"
 const Navbar=()=>{
 return(
   <>
+  <header className={styles1.header}>
   <nav className={styles1.nav}>
   <a href="/Components/home" className="home">Home</a>
 <a href="/Components/about" className="about">About</a>
 <a href="/Components/Menu" className="menu">Menu</a>
 <a href="/Components/FAQ" className="faq">FAQ</a>
   </nav>
+  </header>
   </>
 )
 }
