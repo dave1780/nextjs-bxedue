510fa0eb9521545c69ebe6ce9f5b94e403a91b1e
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-06 16:40:48 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -2,5 +2,5 @@
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
-  res.status(200).json({ name: 'Hello, world!' });
+  res.status(200).json({ name: 'sandeep dhami ' });
 };
