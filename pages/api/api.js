2e7b4b441b16da44d832b470db742fcf71c072fb
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-07 21:47:50 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
-fetch()
+fetch("../")
 };
