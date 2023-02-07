fc3a2159830d8a4ad2dbca7f5f4723a334c7443a
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-07 21:42:32 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
-  console.log(req)
   res.status(200).json({ name: 'sandeep dhami ' });
 };
