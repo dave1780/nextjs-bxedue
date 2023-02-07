273fd2231d065038c61473a908a22531c3024547
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-07 22:03:13 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "fs";
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
-fetch("../")
+  fs.readFile("../../Blogs.json",(error,data )=>{
+    console.log(data)
+  })
 };
