6aa66dd7cf42eac1c7d66640d788f38119fdd8b7
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-18 17:46:36 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 import {MongoClient,ServerApiVersion} from "mongodb"
-
+const uri = "mongo+srv://Dave150:dave90@cluster1.xu4paq5.mongodb.net/?retrywrites=true&w=majority";
+const client = new MongoClient(uri,{
+  usNewUrlParser:true,
+  useUnifiedTopology:true
+});
+client.connect(err =>{
+  const collection = client.db("test").collection("devices");
+  client.close();
+})
+client.once('open',()=>{
+  console.log("connected o mongodb")
+})
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   fs.readFile("../../Blogs.json",(error,data )=>{
