e94dc04d037f9f9b2c4093ee133fc09957b34dca
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-18 11:13:29 +0545

    1 file modified

--- pages/api/api.js
+++ pages/api/api.js
@@ -3,7 +3,7 @@ import * as fs from "fs";
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   fs.readFile("../../Blogs.json",(error,data )=>{
-    console.log(data)
+    console.log("read is on")
   })
   res.status(200).json({name:"dave parker"})
 };
