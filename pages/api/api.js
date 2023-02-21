83c915b3c4ff61dd088bb06a6e58056ada852780
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-21 15:09:25 +0545

    1 file modified, 2 added

--- pages/api/api.js
+++ pages/api/api.js
@@ -15,3 +15,21 @@ mongoose
   });
 
 // Open Chrome DevTools to step through the debugger!
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+});
+const userList = new mongoose.model('UserList', userSchema);
+
+const newUserCreation = async () => {
+  try {
+    const newUser = new userList({
+      name: 'Dave parker',
+      age: 40,
+    });
+    const result = await newUser.save();
+  } catch (err) {
+    console.log(err);
+  }
+};
