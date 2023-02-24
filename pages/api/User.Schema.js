9c82586eaeb4dfb251df7bd6939ffe559aadd700
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-24 18:47:39 +0545

    1 file modified

---
+++ pages/api/User.Schema.js
@@ -0,0 +1,9 @@
+const mongoose= require("mongoose")
+const userSchema= new mongoose.Schema({
+  userName:{type:String , required: true},
+  email:{type:String, required: true},
+  password :{type : String, required:true},
+  confirm_Password:{type:String,required:true}
+})
+
+const userList = new 
