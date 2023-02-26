86e48a76a3cc0d6b16ef719bcec23f97f8f1dde2
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-26 18:36:47 +0545

    1 file added

---
+++ pages/User-Regestration.js
@@ -0,0 +1,17 @@
+const newUser = () => {
+  return (
+    <>
+      <form action="">
+        <input type="text" placeholder="Email:" />
+        <br />
+        <input type="text" placeholder="Password" />
+        <br />
+       Male: <input type="radio" />
+       Female: <input type="radio" />
+        <br />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+export default newUser;
