5933c41abe08b557c248ee2bb7d90c97c508257e
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-02 19:28:56 +0545

    1 file modified

--- pages/api/fs.js
+++ pages/api/fs.js
@@ -1,4 +1,9 @@
 const fs= require("fs")
-fs.writeFile("../../Data.json",[{name:"dave"}],()=>{
+const userData={
+  name: "dave537",
+  number: 2
+}
+const user = JSON.stringify(userData)
+fs.writeFile("../../Data.json",(user),()=>{
   console.log("we have write the data")
 })
