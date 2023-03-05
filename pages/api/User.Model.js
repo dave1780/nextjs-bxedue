ef8217d48b14091b86d205027e9f8805f127255d
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-05 18:52:02 +0545

    1 file modified

--- pages/api/User.Model.js
+++ pages/api/User.Model.js
@@ -1,2 +1,9 @@
 const mongoose= require("mongoose")
-const userlist = require("./User.Schema")
+const userList = require("./User.Schema")
+
+const newUser = new userList({
+  UserName: req.body.UserName,
+  Email: req.body.Email,
+  Password: req.body.Password,
+  Confirm_Password:req.body.Cpassword
+}).save();
