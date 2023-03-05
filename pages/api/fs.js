47f6c025e41c095f6d367ee29bb59c2e22915c0c
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-05 18:37:48 +0545

    1 file deleted

--- pages/api/fs.js
+++
@@ -1,9 +0,0 @@
-const fs= require("fs")
-const userData={
-  name: "dave537",
-  number: 2
-}
-const user = JSON.stringify([userData])
-fs.writeFile("../../Data.json",(user),()=>{
-  console.log("we have write the data")
-})
