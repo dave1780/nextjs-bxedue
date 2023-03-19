341e4b88da8dedd63a37b9dbb6c9e398095181f2
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-03-19 16:27:54 +0545

    1 file modified, 1 added

---
+++ pages/Main.js
@@ -0,0 +1,11 @@
+const Main=()=>{
+  const styleBtn={
+    display:"block",
+    backgroundColor:"red"
+  }
+  return(
+    <button style={styleBtn}>
+      clickMe </button>
+  )
+}
+export default Main;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Navbar from './Navbar'
+import Main from  './Main.js'
 function MyApp({ Component, pageProps }) {
   
   return(
     <>
     <Navbar/>
+    <Main/>
   <Component {...pageProps} />
   </>
   )
